fe164957377b6e3784a67bed3154e0f0ddad1bac
Thanh Tan <tanden1357@gmail.com>
2025-11-17 15:41:01 +0700

    update cors 2

--- src/main/java/com/javaweb/project/config/CorsConfig.java
+++ src/main/java/com/javaweb/project/config/CorsConfig.java
@@ -1,29 +1,34 @@
 package com.javaweb.project.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
+@Configuration
 public class CorsConfig {
+
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList(
-                "https://mysql-oop.vercel.app",
-                "http://localhost:5173",
-                "http://localhost:3000",
-                "https://mysql-oop-git-main-batanvlog2503s-projects.vercel.app"
-        ));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://mysql-oop.vercel.app",
+                                "https://mysql-oop-git-main-batanvlog2503s-projects.vercel.app"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
+
